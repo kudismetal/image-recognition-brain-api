bb60298d4a4d78b5c3506740c60786a8aebfac6f
kudismetal <raito_rider@rocketmail.com>
2019-07-14 18:15:56 +0700

    fix pg connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : 'root',
-        database : 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+  		ssl: true
     }
 });
 
